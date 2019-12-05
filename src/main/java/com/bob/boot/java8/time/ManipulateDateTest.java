e3186e89a46868fd958b67f8f692ccf2ed919f68
boblwboy@gmail.com <boblwboy@gmail.com>
2019-12-05 14:38:32 +0800

    新的日期和时间API

--- src/main/java/com/bob/boot/java8/time/ManipulateDateTest.java
+++ src/main/java/com/bob/boot/java8/time/ManipulateDateTest.java
@@ -1,5 +1,10 @@
 package com.bob.boot.java8.time;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
 /**
  * @author boblw
  * @title: ManipulateDateTest
@@ -10,6 +15,29 @@ package com.bob.boot.java8.time;
 public class ManipulateDateTest {
 
     public static void main(String[] args) {
+
+        System.out.println("=======================新的日期和时间API===============================");
+        System.out.println("===========LocalDate、LocalTime、Instant、Duration以及Period==========");
+
+        System.out.println("====================操纵、解析、格式化日期=========================");
+        //2014-03-18
+        LocalDate date1 = LocalDate.of(2014, 3, 18);
+        //2011-03-18
+        LocalDate date2 = date1.withYear(2011);
+        //2011-03-25
+        LocalDate date3 = date2.withDayOfMonth(25);
+        //2011-09-25
+        LocalDate date4 = date3.with(ChronoField.MONTH_OF_YEAR, 9);
+        //以相对方式修改LocalDate对象的属性
+        //2014-03-18
+        LocalDate localDate1 = LocalDate.of(2014, 3, 18);
+        //2014-03-25
+        LocalDate localDate2 = localDate1.plusWeeks(1);
+        //2011-03-25
+        LocalDate localDate3 = localDate2.minusYears(3);
+        //2011-09-25
+        LocalDate localDate4 = localDate3.plus(6, ChronoUnit.MONTHS);
+
 
     }
 
