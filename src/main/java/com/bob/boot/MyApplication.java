7521b98f57c0f8df3dc7db572480372fcfb010d1
boblwboy@gmail.com <boblwboy@gmail.com>
2019-12-18 14:09:42 +0800

    log

--- src/main/java/com/bob/boot/MyApplication.java
+++ src/main/java/com/bob/boot/MyApplication.java
@@ -1,7 +1,11 @@
 package com.bob.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author boblw
@@ -12,7 +16,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MyApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+    }
+
+    @PostConstruct
+    public void myLog() {
+        LOGGER.trace("Trace Message");
+        LOGGER.debug("Debug Message");
+        LOGGER.info("Info Message");
+        LOGGER.warn("Warn Message");
+        LOGGER.error("Error Message");
     }
 }
